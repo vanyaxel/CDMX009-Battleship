f9faecd5aea1c1022ac8dfc522ba28f001a06e4d
vanyaxel <vanya_xel@live.com.mx>
2020-08-30 21:21:30 -0500

    game functions complete

--- src/components/GameView/GameView.jsx
+++ src/components/GameView/GameView.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Board from '../board/BoardPlayer1';
+import BoardPlayer1 from '../board/BoardPlayer1';
 import BoardPlayer2 from '../board/BoardPlayer2';
-import Btn from '../../components/button/Button';
-import { planetAddedP2 } from '../../store/player2';
+import Winner from '../Winner';
 
+import { getNotGuessedLocationP1 } from '../../store/player1';
+import { getNotGuessedLocationP2 } from '../../store/player2';
 
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import './game-view.css';
 
-import BoardPlayer1 from '../board/Board';
 import ModalFullScreen from '../General-Info/modal-instructions/ModalFullScreen';
 
 import { userAdded } from '../../store/user';
@@ -41,6 +41,16 @@ function GameView() {
 
     const user = useSelector(state => state.entities.user);
 
+    const notGuessedP1 = useSelector(getNotGuessedLocationP1);
+    const notGuessedP2 = useSelector(getNotGuessedLocationP2);
+
+    let statusGameP1 = notGuessedP1.length;
+    let statusGameP2 = notGuessedP2.length;
+
+    console.log('player 1', notGuessedP1);
+    console.log('player 2', notGuessedP2);
+
+
     let userLS = JSON.parse(localStorage.getItem('userPlayer1'));
 
     let namePlayer1 = userLS[0].name;
@@ -48,27 +58,36 @@ function GameView() {
     return (
         <>
             <div className='game-view'>
-                <div className='game-boards'>
-                    <Typography variant="h4" className={classes.nameText}
-                        gutterBottom>
-                        {namePlayer1}
-                    </Typography>
-                    <Board />
-                </div>
-                <div className='game-boards'>
-                    <Typography variant="h4" className={classes.nameTextOpponent} gutterBottom>
-                        Oponente
-                    </Typography>
-                    <BoardPlayer2 />
-                </div>
-                {/* <div>
-                        <Typography variant="h4" className={classes.nameTextOpponent} gutterBottom>
-                            Oponente
-                    </Typography>
-                        <Board />
-                    </div> */}
+                {
+                    statusGameP1 === 1 || statusGameP2 === 1 ?
+                        (
+                            <Winner />
+
+                        ) : (
+                            <>
+                                <div className='game-view'>
+                                    <div className='game-boards'>
+                                        <Typography variant="h4" className={classes.nameText}
+                                            gutterBottom>
+                                            {namePlayer1}
+                                        </Typography>
+                                        <BoardPlayer1 />
+                                    </div>
+                                    <div className='game-boards'>
+                                        <Typography variant="h4" className={classes.nameTextOpponent} gutterBottom>
+                                            Oponente
+                                    </Typography>
+                                        <BoardPlayer2 />
+                                    </div>
+                                    <div>
+                                        <ModalFullScreen />
+                                    </div>
+                                </div>
+                            </>
+                        )
+                }
+
             </div>
-            <ModalFullScreen />
         </>
     );
 }
